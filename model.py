7931b56a53415c5d1c7b1074c72302308ac1d3a7
mehrab khanmohammadi <programmer.py.mail@gmail.com>
2025-10-01 14:04:10 +0330

    the project files and student class (__init__ function to receive data,student is full name , to_dict) were created)

---
+++ model.py
@@ -0,0 +1,38 @@
+import uuid
+import re
+
+
+class Student :
+    def __init__(self,name:str,lastname:str,gender:str,national_code:str,phone:str,email:str=None) :
+        self.id = uuid.uuid4()
+        self.name = name
+        self.lastname = lastname
+        self.gender = gender
+        match = re.fullmatch(r"\d{10}",national_code)
+        if not match :
+            raise ValueError("invalid national code")
+        self.national_code = national_code
+        match = re.fullmatch(r"09\d{9}",phone)
+        if not match :
+            raise ValueError("invalid phone number")
+        self.phone = phone
+        match = re.fullmatch(r"[a-zA-Z0-9_.]+@gmail\.com$",email)
+        if not match :
+            raise ValueError("invalid email")
+        self.email = email if email else ""
+
+    def fullname(self) :
+        return f"{self.name} {self.lastname}"
+
+    def to_dict(self) :
+        return {
+            "id":str(self.id),
+            "name":self.name,
+            "lastname":self.lastname,
+            "gender":self.gender,
+            "national_code":self.national_code,
+            "phone":self.phone,
+            "email":self.email
+        }
+
+
